7919ae9ba9ad8273c06d250096ff6539c85c2e53
SegoviaLourdes <segovia.lourdes1309@gmail.com>
2023-09-07 10:13:53 -0300

    cambio en el home

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const booksJson = fs.readFileSync(path.join(__dirname,"../data/products.json"),"utf-8");
+const books = JSON.parse(booksJson);
+
 const mainController = {
-    home: (req, res) => {
-        res.render('main/home');
-    }
+    home: (req, res)=>{
+        res.render('main/home', {books: books});
+    },
 }
 
 module.exports = mainController;
