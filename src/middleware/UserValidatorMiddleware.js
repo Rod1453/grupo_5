82e067ecfbf95ef92cbf009f4c5c333f6733bef1
Maxi20fi <maxigroxx@gmail.com>
2023-11-15 09:23:39 -0300

    agrego crud de usuarios

--- src/middleware/UserValidatorMiddleware.js
+++ src/middleware/UserValidatorMiddleware.js
@@ -3,12 +3,15 @@ const { body } = require('express-validator');
 const validations = [
     body("nombre")
         .notEmpty().withMessage("Este campo no puede estar vacio."),
-    body("email")
+    body("apellido")
         .notEmpty().withMessage("Este campo no puede estar vacio."),
+    body("email")
+        .notEmpty().withMessage("Este campo no puede estar vacio.").bail()
+        .isEmail().withMessage("debe ingresar un email valido"),
     body("password")
-        .notEmpty().withMessage("Este campo no puede estar vacio.").isLength({ min: 8, max: 25 }).withMessage("la contraseña debe tener al menos 8 caracteres"),
+        .notEmpty().withMessage("Este campo no puede estar vacio.").bail().isLength({ min: 8, max: 25 }).withMessage("la contraseña debe tener al menos 8 caracteres"),
     body("confirmPassword")
-        .notEmpty().withMessage("Este campo no puede estar vacio.").isLength({ min: 8, max: 25 }).withMessage("la contraseña debe tener al menos 8 caracteres")
+        .notEmpty().withMessage("Este campo no puede estar vacio.").bail().isLength({ min: 8, max: 25 }).withMessage("la contraseña debe tener al menos 8 caracteres")
         .custom((value, { req }) => value === req.body.password).withMessage("las contraseñas no coinciden."),
 ];
 
